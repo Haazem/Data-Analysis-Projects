b71920c1865160f41743b5f8263d82c2dbacefee
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-11-01 12:04:20 +0200

    Update Danny's Diner.sql

--- 8-Week-SQL-Challenge/Case Study #1 - Danny's Diner/SQL Code/Danny's Diner.sql
+++ 8-Week-SQL-Challenge/Case Study #1 - Danny's Diner/SQL Code/Danny's Diner.sql
@@ -1,1 +1,13 @@
+# 1. What is the total amount each customer spent at the restaurant?
+
+SELECT customer_id , 
+       SUM(price) as total
+
+FROM dannys_diner.sales s 
+JOIN dannys_diner.menu m 
+ON s.product_id = m.product_id 
+
+GROUP BY customer_id
+
+ORDER BY total DESC;
 
