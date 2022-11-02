2e0e72aabb784102e776eff6abfd14d68bb0e3ed
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-11-02 10:37:29 +0200

    Update Danny's Diner.sql

--- 8-Week-SQL-Challenge/Case Study #1 - Danny's Diner/SQL Code/Danny's Diner.sql
+++ 8-Week-SQL-Challenge/Case Study #1 - Danny's Diner/SQL Code/Danny's Diner.sql
@@ -6,9 +6,7 @@ SELECT customer_id ,
 FROM dannys_diner.sales s 
 JOIN dannys_diner.menu m 
 ON s.product_id = m.product_id 
-
 GROUP BY customer_id
-
 ORDER BY total DESC;
 
 -- 2. How many days has each customer visited the restaurant?
@@ -17,7 +15,7 @@ SELECT customer_id ,
        COUNT(DISTINCT order_date) AS num_days
        
 FROM dannys_diner.sales as s  
-GROUP BY customer_id 
+GROUP BY customer_id ;
 
 -- 3. What was the first item from the menu purchased by each customer?
 
@@ -40,19 +38,17 @@ FROM CTE
 WHERE RN = 1;
 
 
-- 4 What is the most purchased item on the menu and how many times
- was it purchased by all customers?
+-- 4 What is the most purchased item on the menu and how many times
+ --was it purchased by all customers?
 
-SELECT product_name,
+SELECT TOP 1 product_name,
        COUNT(*) AS total_purchased
        
 FROM dannys_diner.menu  m 
 JOIN dannys_diner.sales s 
 ON m.product_id = s.product_id
-
 GROUP BY product_name 
-ORDER BY total_purchased DESC
-LIMIT 1;
+ORDER BY total_purchased DESC;
 
 --5 Which item was the most popular for each customer?
 
@@ -114,35 +110,27 @@ WHERE rk =1;
 
 --7 Which item was purchased just before the customer became a member?
 
+WITH CTE AS 
+(
+   SELECT s.customer_id, m.join_date, s.order_date, s.product_id,
+         RANK() OVER(PARTITION BY s.customer_id
+         ORDER BY s.order_date DESC) AS rank
 
-WITH CTE 
-AS(
-SELECT s.customer_id,
-       s.product_id,
-       product_name,
-       order_date,
-       RANK() OVER(PARTITION BY s.customer_id 
-                   ORDER BY order_date DESC) AS rk
+   FROM dannys_diner.sales AS s
+   JOIN dannys_diner.members AS m
+      ON s.customer_id = m.customer_id
 
-FROM dannys_diner.sales s 
-JOIN dannys_diner.menu m 
-ON s.product_id = m.product_id
-JOIN dannys_diner.members mb 
-ON mb.customer_id = s.customer_id
-
-WHERE s.order_date < mb.join_date 
-ORDER BY s.customer_id 
+   WHERE s.order_date < m.join_date
 )
 
-SELECT customer_id,
-        product_name,
-        order_date
-FROM CTE 
-WHERE rk = 1;
-
+SELECT s.customer_id, s.order_date, m2.product_name 
+FROM CTE AS s
+JOIN dannys_diner.menu AS m2
+   ON s.product_id = m2.product_id
+WHERE rank = 1;
 
 -- 8 What is the total items and amount spent for each member before they became 
-a member?
+--a member?
 
 SELECT s.customer_id,
 	   COUNT(DISTINCT s.product_id) as num_items,
@@ -176,89 +164,27 @@ ORDER BY s.customer_id;
 -- they earn 2x points on all items, not just sushi 
 --how many points do customer A and B have at the end of January?
 
-WITH dates_cte AS 
-(
-   SELECT *, 
-      DATEADD(DAY, 6, join_date) AS valid_date, 
-      EOMONTH('2021-01-31') AS last_date
-   FROM members AS m
+WITH CTE 
+AS(
+SELECT * ,
+       DATEADD(DAY , 6 , join_date) as valid_interval,
+       EOMONTH('2021-01-31')        as interval_end
+FROM dannys_diner.members
 )
 
-SELECT d.customer_id, s.order_date, d.join_date, d.valid_date, d.last_date, m.product_name, m.price,
-   SUM(CASE
-      WHEN m.product_name = 'sushi' THEN 2 * 10 * m.price
-      WHEN s.order_date BETWEEN d.join_date AND d.valid_date THEN 2 * 10 * m.price
-      ELSE 10 * m.price
-      END) AS points
-FROM dates_cte AS d
-JOIN sales AS s
-   ON d.customer_id = s.customer_id
-JOIN menu AS m
-   ON s.product_id = m.product_id
-WHERE s.order_date < d.last_date
-GROUP BY d.customer_id, s.order_date, d.join_date, d.valid_date, d.last_date, m.product_name, m.price
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT 
+	  s.customer_id,
+      SUM(CASE
+		WHEN s.order_date BETWEEN  c.join_date AND c.valid_interval
+		THEN 2*10*m.price
+		WHEN m.product_name = 'sushi' THEN 2*10*m.price
+		ELSE 10*m.price
+		END) AS points
+
+FROM dannys_diner.sales s 
+JOIN CTE c
+ON s.customer_id = c.customer_id
+JOIN dannys_diner.menu m 
+ON m.product_id = s.product_id
+WHERE s.order_date < c.interval_end
+GROUP BY s.customer_id;
